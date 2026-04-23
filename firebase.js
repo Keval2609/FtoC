25be3a3db714b8f565cb52c78542705d0c1602cf
Kevalkumar Raiyani <kevalraiyani2692@gmail.com>
2026-04-23 23:14:48 +0530

    completed phase 2.1 in app

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBcT7Uq3Ip0WsdylMt6WaEBlU7MYDQupyk",
-    authDomain: "gen-lang-client-0069731788.firebaseapp.com",
-    projectId: "gen-lang-client-0069731788",
-    storageBucket: "gen-lang-client-0069731788.firebasestorage.app",
-    messagingSenderId: "987182376699",
-    appId: "1:987182376699:web:8bfdef8f2d6b5b8ef3b25e"
+    apiKey: process.env.VITE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VITE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
