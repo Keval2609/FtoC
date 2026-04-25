05b61b293d03f054a29d9918f0a4851b0b8dc651
Kevalkumar Raiyani <kevalraiyani2692@gmail.com>
2026-04-26 05:27:53 +0530

    added real time database rules in firebase.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getPerformance } from "firebase/performance";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
 
 
 const firebaseConfig = {
@@ -18,5 +19,6 @@ const app = initializeApp(firebaseConfig);
 const perf = getPerformance(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const database = getDatabase(app);
 export const provider = new GoogleAuthProvider();
 export default app;
